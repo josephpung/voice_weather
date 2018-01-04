b23d4e11765f37f3d5c1fade06f0105bf2454fd0
josephpung <goldenpummel@live.com>
2018-01-04 17:19:24 +0800

    added axios and 1 basic command

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Geolocator from './Geolocator'
+import MainPage from './MainPage'
 
 class App extends Component {
   render() {
     return (
-      <Geolocator />
+      <MainPage />
     )
   }
 }
